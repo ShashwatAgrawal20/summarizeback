242761db2923d46be5ce91ee5b117c68febf8524
MrCracker-OP <mr.cracker103@gmail.com>
2024-02-04 00:03:11 +0530

    implemented summarizer for both static and url

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from newspaper import Article
+from transformers import pipeline
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 CORS(app)
@@ -29,6 +31,46 @@ def extract_text():
             400
 
         text_content = extract_text_from_url(url)
+        return jsonify({'text_content': text_content})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+    
+def summarize_text_from_url(url):
+    try:
+        if is_valid_url(url):
+            urltext = extract_text_from_url(url)
+        else:
+         urltext = url
+
+        summarizer = pipeline("summarization")
+        text = urltext
+        summary = summarizer(text, max_length=1000, min_length=100, length_penalty=2.0)
+        summarycontent = summary[0]['summary_text']
+        return summarycontent
+    except Exception as e:
+            return f"Error summarizing text: {str(e)}"
+
+
+@app.route('/summarize-text', methods=['POST'])
+def summarize_text():
+    try:
+        data = request.get_json()
+        url = data.get('url')
+
+        if not url:
+            return jsonify(
+                {'error': 'Please provide a valid text content in the request body'}),
+            400
+
+        text_content = summarize_text_from_url(url)
         return jsonify({'text_content': text_content})
 
     except Exception as e:
