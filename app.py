d4f78632c647da837500d6df01a7c0131fa995df
Shashwat Agrawal <72117025+ShashwatAgrawal20@users.noreply.github.com>
2024-04-10 21:35:49 +0530

    feat(core): summarization from keyword. (#6)
    * feat(core): summarization from keyword.
    Not sure why but sometimes when using the keyword summarization feature
    this shit breaks with error like `Connection Closed`.
    
    - using the `wikipedia module` to get the url of a page regarding the
      keyword and then creating a HTMLParser out of that url.
    
    - modifying the endpoint to take an additional param called keyword.
    
    - this shit is not currently full proof as it's currently only based on
      the wikipedia content.
    
    * fix(core): removing unwanted print statement used for debugging.

--- app.py
+++ app.py
@@ -11,17 +11,28 @@ from sumy.utils import get_stop_words
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from wikipedia import page, exceptions
 
 app = Flask(__name__)
 CORS(app)
 LANGUAGE = "english"
 
 
-def get_summary(url=None, text=None, sentences_count=None):
+def get_summary(url=None, text=None, keyword=None, sentences_count=None):
     if url:
         parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-    else:
+    elif text:
         parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
+    else:
+        try:
+            wiki_url = page(keyword).url
+        except exceptions.DisambiguationError as e:
+            wiki_url = page(e.options[0]).url
+        except exceptions.PageError:
+            return jsonify({"error": f"No Wikipedia \
+            page found for keyword: {keyword}"}), 404
+
+        parser = HtmlParser.from_url(wiki_url, Tokenizer(LANGUAGE))
 
     stemmer = Stemmer(LANGUAGE)
     summarizer = Summarizer(stemmer)
@@ -40,12 +51,13 @@ def main():
         length = data.get("length") or 15
         url = data.get("url")
         raw_text = data.get("text")
+        keyword = data.get("keyword")
 
-        if not url and not raw_text:
-            return jsonify({"error": "URL or text is required"}), 400
+        if not url and not raw_text and not keyword:
+            return jsonify({"error": "URL, text or keyword is required"}), 400
 
         summarized_text = get_summary(
-            url=url, text=raw_text, sentences_count=length)
+            url=url, text=raw_text, keyword=keyword, sentences_count=length)
 
         return jsonify({"summarized_text": summarized_text})
 
